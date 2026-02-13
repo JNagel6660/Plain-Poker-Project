f2d4da1faf3ee0a70a47930c8ec680c87d2d292a
JNagel6660 <Jonathann175@nycstudents.net>
2026-02-13 14:33:53 -0500

    Add files via upload

--- Main.java
+++ Main.java
@@ -30,47 +30,23 @@ public class Main {
 
         hand[] allHands = new hand[lines.length];
         int[] rankings = new int[lines.length];
+        String[] hand1 = new String[4];
+        String[] hand2 = new String[4];
+        String[] allBids = new String[lines.length];
 
 
         int lineNumber = 0;
         int rankDummy;
+        int lineDummy;
         for (String line : lines ) {
             String[] numbers = line.split("\\|");
             System.out.println(Arrays.toString(numbers));
             String[] cards = numbers[0].split(",");
-            String bid = numbers[1];
+            allBids[lineNumber] = numbers[1];
             hand h = new hand(cards);
             h.handStrengthCalc();
             allHands[lineNumber] = h;
             System.out.println(h.handStrengthCalc());
-            for (int i = lineNumber; i < allHands.length; i++) {
-                rankings[i] = lines.length - i;
-                if (lineNumber == 0){
-                    i += allHands.length;
-                } else{
-                    // take this one hand and compare it to the previous x:
-                    for (int j = lineNumber - 1; j > 0; j--) {
-                        if(allHands[j].getHandPower() > allHands[lineNumber].getHandPower() ){
-                            j = 0;
-                        } else if (allHands[j].getHandPower() == allHands[lineNumber].getHandPower()){
-
-
-                        } else {
-                            // make the hand being looked at trade ranks with the previous hand.
-                            rankDummy = rankings[lineNumber];
-                            rankings[lineNumber] = rankings[j];
-                            rankings[j] = lineNumber;
-
-                        }
-
-                    }
-
-                    }
-
-                }
-
-
-
             if (h.handStrengthCalc().equals(" Five of a kind")){
                 fiveOAK += 1;
             }
@@ -96,6 +72,85 @@ public class Main {
             lineNumber++;
         }
 
+        for (int i = 0; i < allHands.length; i++) {
+            for (int j = i + 1; j < allHands.length; j++) {
+                if(allHands[i].getHandPower() > allHands[j].getHandPower()){
+                    System.out.println("hello");
+                    rankings[i] += 1;
+                } else if (allHands[i].getHandPower() == allHands[j].getHandPower()) {
+                    hand1 = allHands[i].getSpecificHand();
+                    hand2 = allHands[j].getSpecificHand();
+                    for (int k = 0; k < hand1.length; k++) {
+                        if (hand1[k].equals("Ace") && !hand2[k].equals("Ace")){
+                            rankings[i] += 1;
+                            k = 1000;
+                        } else if ((hand1[k].equals("King") && (!hand2[k].equals("King")) && !hand2[k].equals("Ace"))){
+                            rankings[i] += 1;
+                            k = 1000;
+                        } else if ((hand1[k].equals("Queen") && ((!hand2[k].equals("Queen")) && (!hand2[k].equals("Ace") && !hand2[k].equals("King"))))) {
+                            rankings[i] += 1;
+                            k = 1000;
+                        } else if (hand1[k].equals("Jack") && (!hand2[k].equals("Jack"))) {
+                            k = 1000;
+                        } else if (hand1[k].compareTo(hand2[k]) > 0){
+                            rankings[i] += 1;
+                            k = 1000;
+                        } else if (hand1[k].compareTo(hand2[k]) < 0) {
+                            k = 1000;
+                        }
+
+                    }
+
+
+
+                }
+            }
+            for (int k = i - 1; k >= 0 ; k--) {
+                if(allHands[i].getHandPower() > allHands[k].getHandPower()){
+                    rankings[i] += 1;
+                    System.out.println("hello");
+                } else if (allHands[i].getHandPower() == allHands[k].getHandPower()) {
+                    hand1 = allHands[i].getSpecificHand();
+                    hand2 = allHands[k].getSpecificHand();
+                    for (int p = 0; p < hand1.length; p++) {
+                        if (hand1[p].equals("Ace") && !hand2[p].equals("Ace")){
+                            rankings[i] += 1;
+                            p = 1000;
+                        } else if ((hand1[p].equals("King") && (!hand2[p].equals("King")) && !hand2[p].equals("Ace"))){
+                            rankings[i] += 1;
+                            p = 1000;
+                        } else if (((hand1[p].equals("Queen")) && ((!hand2[p].equals("Queen"))) && (!hand2[p].equals("Ace") && !hand2[p].equals("King")))) {
+                            rankings[i] += 1;
+                            p = 1000;
+                        } else if (hand1[p].equals("Jack") && (!hand2[p].equals("Jack"))) {
+                            p = 1000;
+                        } else if (hand1[p].compareTo(hand2[p]) > 0){
+                            rankings[i] += 1;
+                            p = 1000;
+                        } else if (hand1[p].compareTo(hand2[p]) < 0) {
+                            p = 1000;
+                        }
+
+                    }
+
+
+                }
+
+
+            }
+            rankings[i] += 1;
+
+
+
+        }
+        int bidVal = 0;
+        int total = 0;
+        for (int i = 0; i < rankings.length; i++) {
+            bidVal = Integer.parseInt(allBids[i]);
+            rankings[i] = rankings[i] * bidVal;
+            total += rankings[i];
+        }
+
         System.out.println(Arrays.toString(rankings));
         System.out.println("Number of five of a kinds = " + fiveOAK);
         System.out.println("Number of four of a kinds = " + fourOAK);
@@ -104,5 +159,6 @@ public class Main {
         System.out.println("Number of two pairs = " + twoPair);
         System.out.println("Number of pairs = " + pair);
         System.out.println("Number of high cards = " + highCard);
+        System.out.println("total bid values with jack wild = " + total);
     }
 }
--- hand.java
+++ hand.java
@@ -17,9 +17,14 @@ public class hand {
         return handPower;
     }
 
+    public String[] getSpecificHand(){
+        return specificHand;
+    }
+
     public String handStrengthCalc() {
         int count[] = {0, 0, 0, 0};
         int finalCount = 0;
+        int jackCount = 0;
         for (int i = 1; i < specificHand.length; i++) {
             if(specificHand[0].equals(specificHand[i])){
                 count[0] += 1;
@@ -46,36 +51,78 @@ public class hand {
         }
         for (int i = 0; i < count.length; i++) {
             finalCount += count[i];
-            if(count[i] == 3) {
+            if (specificHand[i].equals("Jack")) {
+                jackCount += 1;
+            }
+            if (jackCount == 4) {
+                this.handPower = 6;
+                return " Five of a kind";
+            }
+        }
+        if (finalCount == 6){
+            if (jackCount == 1){
+                this.handPower = 6;
+                return " Five of a kind";
+            }else {
                 this.handPower = 5;
                 return " four of a kind";
             }
         }
+            System.out.println(jackCount);
+
+
         if (finalCount == 4){
+            if (jackCount == 0){
             this.handPower = 4;
             return " Full house";
         }
+            if (jackCount == 2 || jackCount == 3){
+                this.handPower = 6;
+                return " Five of a kind";
+            }
+
+        }
         if(finalCount == 3){
-            this.handPower = 3;
-            return " three of a kind";
+            if (jackCount == 1 || jackCount == 2 || jackCount == 3){
+                this.handPower = 5;
+                return " four of a kind";
+            } else {
+                this.handPower = 3;
+                return " three of a kind";
+            }
+
         }
         if(finalCount == 2){
-            this.handPower = 2;
-            return " two pair";
+            if (jackCount == 2){
+                this.handPower = 5;
+                return " four of a kind";
+            } else if (jackCount == 1) {
+                this.handPower = 4;
+                return " Full house";
+
+            }else {
+                this.handPower = 2;
+                return " two pair";
+            }
         }
-        if (finalCount == 1){
-            this.handPower = 1;
-            return " pair";
+        if (finalCount == 1) {
+            if (jackCount == 1) {
+                this.handPower = 3;
+                return " three of a kind";
+            } else if (jackCount == 2) {
+                this.handPower = 3;
+                return " three of a kind";
+            } else {
+                this.handPower = 1;
+                return " pair";
+            }
         }
         else {
             this.handPower = 0;
             return " high card";
         }
     }
-    public int compareHands(hand a, hand b){
 
-
-    }
 
 
 
